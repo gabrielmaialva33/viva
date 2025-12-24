fc3a78211c19532c3eec72ad2c9670cebfdd4022
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-12-24 19:19:43 -0300

    chore: remove old diagnostic, testing, and static analysis scripts

--- run_credo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PATH=$PATH:/usr/local/bin:/opt/homebrew/bin
-mix deps.get
-mix credo --strict --format=json > credo_results.json 2> credo_error.log
-ls -l credo_results.json
-cat credo_error.log
